5998d7a781f98fefa5c4b4eb93f2638a8b4d2209
evakobets2004 <148678806+evakobets2004@users.noreply.github.com>
2023-10-22 10:47:24 +0300

    submission_template01_kobetc.py

--- Deep Learning/submission_template01.py
+++ Deep Learning/submission_template01.py
@@ -3,13 +3,25 @@ import torch
 from torch import nn
 
 def create_model():
-    # your code here
-    # return model instance (None is just a placeholder)
+    model = nn.Sequential(
+        nn.Linear(784, 256),
+        nn.ReLU(),
+        nn.Linear(256, 16),
+        nn.ReLU(),
+        nn.Linear(16, 10)
+    )
 
-    return None
+    return model
+model = create_model()
 
 def count_parameters(model):
-    # your code here
-    # return integer number (None is just a placeholder)
+    return sum(p.numel() for p in model.parameters())
+
+small_model = nn.Linear(128, 256)
+print(count_parameters(small_model))
+
+medium_model = nn.Sequential(*[nn.Linear(128, 32, bias=False), nn.ReLU(), nn.Linear(32, 10, bias=False)])
+print(count_parameters(medium_model))
+
     
-    return None
+    
